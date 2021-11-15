2b18afbf7459b541aae4dbc44845e8e3373ff393
murage <murage@labskenya.com>
2021-11-15 16:19:42 +0300

    feat: make copyright year dynamic

---
+++ assets/ts/shared.ts
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const navbar = document.querySelector('nav.navbar');
+    if (navbar) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 55) {
+                navbar.classList.add('opaque')
+            } else {
+                navbar.classList.remove('opaque')
+            }
+        })
+    }
+
+
+    const elems: NodeListOf<HTMLElement> = document.querySelectorAll('.current-year');
+    const year = new Date().getFullYear()
+    console.log(year)
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].innerText = String(year);
+    }
+
+})
